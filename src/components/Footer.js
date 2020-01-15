3a4d5ffacf462e4d999ebe5b82f6dfcd9189b302
daltonlcarr <daltcarr@gmail.com>
2020-01-15 15:46:17 -0800

    copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">Dalton Carr &copy;></p>
+        <p className="copyright">Dalton Carr &copy;</p>
     </footer>
 )
 
